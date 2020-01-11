3715e9139b1afee818d19c7782b75f0e652dd43d
Diego Oliveira da Silva <diegolinkk@gmail.com>
2020-01-11 01:13:06 -0300

    funcionalidade de ensaiar ou tocar os louvores

--- songs/urls.py
+++ songs/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('agitados/',views.agitados,name='agitados'),
+    path('adoracao/',views.adoracao,name='adoracao'),
     path('bandas/',views.bandas,name='bandas'),
     path('bandas/<int:id_banda>/',views.banda_musicas,name='banda_musicas'),
+    path('ensaio/',views.ensaio,name='ensaio'),
+    path('ensaiar/<int:id_louvor>/',views.ensaiar,name='ensaiar'),
+    path('tocar/<int:id_louvor>/',views.tocar,name='tocar'),
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Louvor,Banda
 
 # Create your views here.
@@ -8,6 +9,20 @@ def index(request):
     louvores = Louvor.objects.all()
     context = {
         "louvores": louvores,
+    }
+    return render(request,'songs/song_list.html',context=context)
+
+def agitados(request):
+    louvores = Louvor.objects.filter(estilo='agitado')
+    context = {
+    "louvores": louvores,
+    }
+    return render(request,'songs/song_list.html',context=context)
+
+def adoracao(request):
+    louvores = Louvor.objects.filter(estilo='adoracao')
+    context = {
+    "louvores": louvores,
     }
     return render(request,'songs/song_list.html',context=context)
 
@@ -25,3 +40,23 @@ def banda_musicas(request,id_banda):
         "louvores": louvores,
     }
     return render(request,'songs/song_list.html',context=context)
+
+def ensaio(request):
+    louvores = Louvor.objects.all().order_by('vezes_tocada','vezes_ensaiada')
+    context = {
+        "louvores": louvores,
+    }
+    return render(request,'songs/song_register.html',context=context)
+
+def ensaiar(request,id_louvor):
+    louvor = Louvor.objects.get(id=id_louvor)
+    louvor.vezes_ensaiada +=1
+    louvor.save()
+    return HttpResponseRedirect(reverse('ensaio'))
+
+
+def tocar(request,id_louvor):
+    louvor = Louvor.objects.get(id=id_louvor)
+    louvor.vezes_tocada +=1
+    louvor.save()
+    return HttpResponseRedirect(reverse('ensaio'))
